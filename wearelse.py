246a65600a3ed9b1bd02083b7cc99c24db7eec88
programmingprincess <jiaqiwu@asu.edu>
2019-10-26 13:43:53 -0500

    flask render template and static files

--- wearelse.py
+++ wearelse.py
@@ -1,6 +1,19 @@
-from flask import Flask
+from flask import Flask,render_template,url_for
 app = Flask(__name__)
 
+# @app.route('/')
+# def hello_word():
+# 	return 'Hello, World!'
+
 @app.route('/')
-def hello_word():
-	return 'Hello, World!'
+def hello(name=None):
+    return render_template('index.html', name=name)
+
+'''
+Items list has JSON object of 
+img, title, price for each item.
+'''
+@app.route('/results')
+def query(items_list=None):
+	print(url_for('static', filename='main.css'))
+	return render_template('hello.html', items=items_list, main_css=url_for('static', filename='main.css'))
