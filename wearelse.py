b9434106d0bfbf1acf50aec533c3a94211d4147d
programmingprincess <jiaqiwu@asu.edu>
2019-10-26 14:34:13 -0500

    templating for results

--- wearelse.py
+++ wearelse.py
@@ -1,6 +1,17 @@
 from flask import Flask,render_template,url_for
 app = Flask(__name__)
 
+test = '''
+[
+	{"title": "Black turtleneck sweater", "description": "100% cotton, from H&M!", "link": "http://google.com", 
+    "img_url": "http://jiaqiwu.com/portrait_2019.jpeg", "price": 8.99, "sale_price": 8.99}, 
+  {"title": "Purple turtleneck sweater", "description": "100% cotton, from H&M!", "link": "http://google.com", 
+    "img_url": "http://jiaqiwu.com/portrait_2019.jpeg", "price": 8.99, "sale_price": 8.99}, 
+  {"title": "Pink turtleneck sweater", "description": "100% cotton, from H&M!", "link": "http://google.com", 
+    "img_url": "http://jiaqiwu.com/portrait_2019.jpeg", "price": 8.99, "sale_price": 8.99}, 
+]
+'''
+
 # @app.route('/')
 # def hello_word():
 # 	return 'Hello, World!'
@@ -14,6 +25,7 @@ Items list has JSON object of
 img, title, price for each item.
 '''
 @app.route('/results')
-def query(items_list=None):
-	print(url_for('static', filename='main.css'))
-	return render_template('hello.html', items=items_list, main_css=url_for('static', filename='main.css'))
+def get_search_results(items_list=None):
+	
+	return render_template('results.html', items=test)
+
