c6fafb5b6f28de9f15c331b37947304afb8cd040
sushazad <azad.sushmita@gmail.com>
2019-10-26 15:05:09 -0500

    Rigging up flask for index

--- wearelse.py
+++ wearelse.py
@@ -1,57 +1,28 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+import random
 import requests
 import configparser
 import json
+from apis import getNordstromResults, getEtsyResults
 
 app = Flask(__name__)
 
-# @app.route('/')
-# def hello_word():
-# 	return 'Hello, World!'
 
 @app.route('/')
 def hello(name=None):
     return render_template('index.html', name=name)
 
-def getEbayResults(query):
-	URL = "https://openapi.etsy.com/v2/listings/active"
 
-	parser = configparser.ConfigParser()
-	try:
-		parser.read('config.ini')
-		PARAMS = {"api_key": parser.get('ebay_api', 'api_key'),
-	#PARAMS = {"api_key": "xhshosdpo1y0alvwy572r4gc",
-			"keywords": query,
-			"category": "Clothing",
-			"limit": 5}
-		r = requests.get(url = URL, params = PARAMS)
+@app.route('/handle_data', methods=['GET'])
+def handle_data():
+	# print(request.args['query'])
+    #projectpath = request.form['projectFilepath']
+	print(request.args.get('query'))
+	nordstromResults = json.loads(getNordstromResults(request.args.get('query')))
+	etsyResults = json.loads(getEtsyResults(request.args.get('query')))
 
-		data = r.json()
-		print(data)
-		res = []
-		for d in data['results']:
-			URL2 = "https://openapi.etsy.com/v2/listings/" + str(d['listing_id']) + "/images"
-			PARAMS2 = {"api_key": "xhshosdpo1y0alvwy572r4gc"}
-			r2 = requests.get(url = URL2, params = PARAMS2)
-			data2 = r2.json()
-			
-			print(d['title'])
-			print(d['description'])
-			print(d['price'])
-			print(d['url'])
-			print(data2['results'][0]['url_570xN'])
-
-			res.append({"title": d['title'],
-				"description": d['description'],
-				"price": d['price'],
-				"link": d['url'],
-				"img_url": data2['results'][0]['url_570xN']})
-
-		return json.dumps({"results": res})
-	except:
-		print("ERROR: config.ini not created or request is bad")
-
-
+	total = nordstromResults["results"] + etsyResults["results"]
+	return str(total)
 '''
 Items list has JSON object of 
 img, title, price for each item.
